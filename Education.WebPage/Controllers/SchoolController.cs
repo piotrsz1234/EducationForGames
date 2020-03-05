5ac777273eb57b1c0d4d5a8e328108b51d1e3510
Piotr Szuflicki <piotrsz1098@gmail.com>
2020-03-05 15:57:54 +0100

    Added controller for school, with Index, and it's view

---
+++ Education.WebPage/Controllers/SchoolController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EducationLib.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Education.WebPage.Controllers {
+	public class SchoolController : Controller {
+		public IActionResult Index () {
+			return View (HttpContext.Session.Get<User>("User").ID);
+		}
+	}
+}
